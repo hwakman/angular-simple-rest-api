ee7947d0791a944ab7bf7e25a1806448645b5350
Chayoud <chayoud.p@arms-thai.com>
2019-01-11 14:09:50 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import {FormGroup, FormControl } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
@@ -64,7 +66,11 @@ export class AppComponent {
     const a = prompt('Please enter new value.', value);
     const index = this.stock.indexOf(value);
     if (a !== '' && a) {
-      this.stock[index] = a;
+      if (a.length <= 10) {
+        this.stock[index] = a;
+      } else {
+        alert('Max character is (10)');
+      }
     }
   }
   removeAll = () => {
@@ -72,5 +78,24 @@ export class AppComponent {
     if (a === 'clear') {
       this.stock = [];
     }
+  }
+
+  exportPdf = () => {
+    const data = document.getElementById('exportTable');
+    const printBTN = document.getElementById('printBTN');
+    printBTN.style.display = 'none';
+    html2canvas(data).then(canvas => {
+      const imgWidth = 208;
+      const pageHeight = 295;
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+      const heightLeft = imgHeight;
+
+      const imgUrl = canvas.toDataURL('image/png');
+      const pdf = new jspdf('p', 'mm', 'a4');
+      const position = 0;
+      pdf.addImage(imgUrl, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save('example.pdf');
+      printBTN.style.display = 'block';
+    });
   }
 }
