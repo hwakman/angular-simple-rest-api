008b2b6d0db17c9e3310d3d6b40928e5b8e06bc8
Chayoud <chayoud.p@arms-thai.com>
2019-01-11 16:17:04 +0700

    update code add style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ import html2canvas from 'html2canvas';
 export class AppComponent {
 
   name = new FormControl('');
+  searchValue = new FormControl('');
 
   title = 'e-comp';
   sample = [
@@ -37,6 +38,8 @@ export class AppComponent {
 
   stock = [];
 
+  stockTable = [];
+
   showClick = name => {
    const word = 'Hello ' + name;
    alert(word);
@@ -45,29 +48,30 @@ export class AppComponent {
     const check = this.stock.indexOf(this.name.value);
     if (this.name.value) {
       if (check <= -1) {
-        this.stock.push(this.name.value);
+        this.stock.push({id: (this.stock.length + 1), name: this.name.value, code: 'ST-IP-19/01-00' + (this.stock.length + 1)});
         this.name.setValue('');
         document.getElementById('inputAD').focus();
       } else {
         alert('This value already exit !!!');
       }
     } else {
-      this.stock.push('Autodata-00' + (this.stock.length + 1));
+      this.stock.push({id: (this.stock.length + 1), name: 'Autodata-00' + (this.stock.length + 1), code: 'ST-IP-19/01-00' + (this.stock.length + 1)});
       document.getElementById('inputAD').focus();
     }
+    this.stockTable = this.stock;
     console.log(this.stock);
   }
   delState = value => {
-    alert('Remove " ' + value + ' "');
+    alert('Remove " ' + value.name + ' "');
     const index = this.stock.indexOf(value);
     this.stock.splice(index, 1);
   }
   editState = value => {
-    const a = prompt('Please enter new value.', value);
+    const a = prompt('Please enter new value.', value.name);
     const index = this.stock.indexOf(value);
     if (a !== '' && a) {
       if (a.length <= 10) {
-        this.stock[index] = a;
+        this.stock[index].name = a;
       } else {
         alert('Max character is (10)');
       }
@@ -81,21 +85,42 @@ export class AppComponent {
   }
 
   exportPdf = () => {
-    const data = document.getElementById('exportTable');
-    const printBTN = document.getElementById('printBTN');
-    printBTN.style.display = 'none';
-    html2canvas(data).then(canvas => {
-      const imgWidth = 208;
-      const pageHeight = 295;
-      const imgHeight = canvas.height * imgWidth / canvas.width;
-      const heightLeft = imgHeight;
+    if (this.stock.length > 0) {
+      const data = document.getElementById('exportTable');
+      const printHead = document.getElementById('printHead');
+      const printFoot = document.getElementById('printFoot');
+      printHead.classList.add('notShow');
+      printFoot.classList.remove('notShow');
+      html2canvas(data).then(canvas => {
+        const imgWidth = 208;
+        const pageHeight = 295;
+        const imgHeight = canvas.height * imgWidth / canvas.width;
+        const heightLeft = imgHeight;
 
-      const imgUrl = canvas.toDataURL('image/png');
-      const pdf = new jspdf('p', 'mm', 'a4');
-      const position = 0;
-      pdf.addImage(imgUrl, 'PNG', 0, position, imgWidth, imgHeight);
-      pdf.save('example.pdf');
-      printBTN.style.display = 'block';
-    });
+        const imgUrl = canvas.toDataURL('image/png');
+        const pdf = new jspdf('p', 'mm', 'a4');
+        const position = 0;
+        pdf.addImage(imgUrl, 'PNG', 0, position, imgWidth, imgHeight);
+        pdf.save('example.pdf');
+        printHead.classList.remove('notShow');
+        printFoot.classList.add('notShow');
+      });
+    }
+  }
+
+  exportTxt = () => {
+    if (this.stock.length > 0) {
+      const word = [];
+      this.stock.map(data => word.push('ID: ' + data.id + ' | Name: ' + data.name + ' | Code: ' + data.code));
+      const pdf = new jspdf();
+      pdf.text(word, 10, 10);
+      pdf.save('text.pdf');
+    }
+  }
+
+  searchTable = () => {
+    let input;
+    input = this.searchValue.value;
+    console.log(input);
   }
 }
